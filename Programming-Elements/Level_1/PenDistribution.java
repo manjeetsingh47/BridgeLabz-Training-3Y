36a3317c834969afcfe83a044bb305f7bb5213c8
manjeetsingh47 <7347nmanjeet@gmail.com>
2025-08-14 16:35:52 +0530

    [manjeet] added : added programing-elements

---
+++ Programming-Elements/Level_1/PenDistribution.java
@@ -0,0 +1,16 @@
+package Level_1;
+
+public class PenDistribution {
+    public static void main(String[] args) {
+        int totalPens = 14;
+        int students = 3;
+
+        int pensPerStudent = totalPens / students;        // Division operator
+        int remainingPens = totalPens % students;         // Modulus operator
+
+        System.out.println("The Pen Per Student is " + pensPerStudent + 
+                           " and the remaining pen not distributed is " + remainingPens);
+    }
+}
+
+
---
+++ Programming-Elements/Level_1/SamAverage.java
@@ -0,0 +1,17 @@
+package Level_1;
+
+public class SamAverage {
+    public static void main(String[] args) {
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
+
+        // Calculate average
+        double average = (maths + physics + chemistry) / 3.0;
+
+        // Output the result
+        System.out.println("Sam’s average mark in PCM is " + average);
+    }
+}
+
+
