5c82cd51b6b5b158ff480fc2069c38fec8cb4acd
manjeetsingh47 <7347nmanjeet@gmail.com>
2025-08-24 20:56:08 +0530

    [manjeet] - add StringLengthDemo.java code

---
+++ String/level_2/StringLengthDemo.java
@@ -0,0 +1,31 @@
+package String.level_2;
+
+import java.util.Scanner;
+
+public class StringLengthDemo {
+
+    public static int GetLengthWithoutBuiltIn(String Text) {
+        int Count = 0;
+        try {
+            while (true) {
+                Text.charAt(Count);
+                Count++;
+            }
+        } catch (StringIndexOutOfBoundsException e) {
+            return Count;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner ScannerObj = new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        String UserInput = ScannerObj.next();
+
+        int ManualLength = GetLengthWithoutBuiltIn(UserInput);
+        int BuiltInLength = UserInput.length();
+
+        System.out.println("Manual Length:  " + ManualLength);
+        System.out.println("Built-in Length: " + BuiltInLength);
+    }
+}
+
