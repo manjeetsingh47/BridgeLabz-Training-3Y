66a43f2f707078ea61903c6346f2d2a1f4ee1326
manjeetsingh47 <7347nmanjeet@gmail.com>
2025-09-03 15:45:43 +0530

    [manjeet].added Student.java code

---
+++ oops/level_2/Student.java
@@ -0,0 +1,44 @@
+package oops.level_2;
+
+    class Student {
+    String name;
+    int rollNumber;
+    int marks;
+
+    Student(String name, int rollNumber, int marks) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.marks = marks;
+    }
+
+    String calculateGrade() {
+        if (marks >= 90) {
+            return "A";
+        } else if (marks >= 80) {
+            return "B";
+        } else if (marks >= 70) {
+            return "C";
+        } else if (marks >= 60) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+
+    void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Marks: " + marks);
+        System.out.println("Grade: " + calculateGrade());
+    }
+
+    public static void main(String[] args) {
+        Student student1 = new Student("Alice", 101, 85);
+        student1.displayDetails();
+
+        Student student2 = new Student("Bob", 102, 73);
+        student2.displayDetails();
+    }
+}
+
+
