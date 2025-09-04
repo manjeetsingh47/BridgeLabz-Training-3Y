94e1ce8863a60d96225670cc05f3919767b97a14
manjeetsingh47 <7347nmanjeet@gmail.com>
2025-09-04 10:24:06 +0530

    [manjeet].added Book.java code

---
+++ constructor/level_1/Book.java
@@ -0,0 +1,20 @@
+package Constructor.level_1;
+
+public class Book {
+    String title;
+    String author;
+    double price;
+
+    public Book() {
+        title = "";
+        author = "";
+        price = 0.0;
+    }
+
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+}
+
